97f7fe61a8ef21e82f7073d9d4a54f74aa3b4261
Lucid Han <lucidman.han@gmail.com>
2019-03-02 22:48:39 +0900

    작성

--- src/App.js
+++ src/App.js
@@ -1,28 +1,89 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import PageTemplate from "./components/PageTemplate/PageTemplate";
+import TodoInput from "./components/TodoInput/TodoInput";
+import TodoList from "./components/TodoList/TodoList";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    state = {
+        input: '',
+        todos: [
+            {id: 0, text: '리액트 공부하기', done: true},
+            {id: 1, text: '컴포넌트 스타일링 해보기', done: false}
+        ]
+    };
+
+    id = 1;
+    getId = () => {
+        return ++this.id;
+    }
+
+    handleChange = (e) => {
+        const {value} = e.target;
+        this.setState({
+            input: value
+        })
+    };
+
+    handleInsert = () => {
+        const {todos, input} = this.state;
+        // 새 데이터 객체 생성
+        const newTodo = {
+            text: input,
+            done: false,
+            id: this.getId()
+        };
+        this.setState({
+            todos: [...todos, newTodo],
+            input: ''
+        });
+    };
+
+    handleToggle = (id) => {
+        const {todos} = this.state;
+        const index = todos.findIndex(todo => todo.id === id);
+        const toggled = {
+            ...todos[index],
+            done: !todos[index].done
+        };
+        this.setState({
+            todos: [
+                ...todos.slice(0, index),
+                toggled,
+                ...todos.slice(index + 1, todos.length)
+            ]
+        });
+    };
+
+    handleRemove = (id) => {
+        const {todos} = this.state;
+        const index = todos.findIndex(todo => todo.id === id);
+
+        this.setState({
+            todos : [
+                ...todos.slice(0, index),
+                ...todos.slice(index + 1, todos.length)
+            ]
+        });
+    };
+
+    render() {
+
+        const {input, todos} = this.state;
+        const {
+            handleChange, handleInsert, handleToggle, handleRemove
+        } = this;
+
+        return (
+            <div className="App">
+                <PageTemplate>
+                    <TodoInput onChange={handleChange} onInsert={handleInsert} value={input}/>
+                    <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
+                </PageTemplate>
+            </div>
+        );
+    }
 }
 
 export default App;
